64d1b0acfd6a23a9b45d103714689f2dd450e994
Ángela <asanrobb@ieszaidinvergeles.org>
2025-10-16 12:05:25 +0200

    Correction (Ángela)
    The teacher explain to us the parts that we have to change of our practice. I've created a Documentation folder for the files of Memory and Tasks. Moreover I improve
    the code in order to the comments of our teacher, deleting comments, difficult and non necesary code.

--- provision.sh
+++ provision.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
+set -eux
 
+apt-get update
+apt-get install -y isc-dhcp-server
 
-echo ">>> Iniciando el aprovisionamiento del servidor DHCP..."
-
-echo ">>> Actualizando paquetes e instalando isc-dhcp-server..."
-apt-get update >/dev/null 2>&1
-apt-get install -y isc-dhcp-server >/dev/null 2>&1
-
-
-echo ">>> Configurando el servicio DHCP..."
 mv /tmp/dhcpd.conf /etc/dhcp/dhcpd.conf
 mv /tmp/isc-dhcp-server /etc/default/isc-dhcp-server
 
@@ -16,12 +11,4 @@ mv /tmp/isc-dhcp-server /etc/default/isc-dhcp-server
 chown root:root /etc/dhcp/dhcpd.conf
 chown root:root /etc/default/isc-dhcp-server
 
-
-echo ">>> Reiniciando el servicio DHCP..."
 systemctl restart isc-dhcp-server.service
-
-
-echo ">>> Verificando estado del servicio isc-dhcp-server..."
-systemctl is-active --quiet isc-dhcp-server && echo ">>> El servicio DHCP está ACTIVO y funcionando." || echo ">>> ¡ERROR! El servicio DHCP falló al iniciar."
-
-echo ">>> Aprovisionamiento del servidor DHCP completado."
